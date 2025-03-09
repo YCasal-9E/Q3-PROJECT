81f931e6323e5f9a0fcd6ed5bfb085e0c991101d
YCasal-9E <casal.ca@obmontessori.edu.ph>
2025-03-09 11:31:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -50,10 +50,7 @@ function submitForm() {
     alert("This is noted, " + name + ".");
 }
 
-function capitalizeFirstLetter(input) {
-    let words = input.value.split(" ");
-    for (let i = 0; i < words.length; i++) {
-        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+function capitalizeFirstLetter(id) {
+        var input = document.getElementById(id); 
+        input.value = input.value.charAt(0).toUpperCase() + input.value.slice(1);
     }
-    input.value = words.join(" ");
-}
