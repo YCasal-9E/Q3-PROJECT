0d556007a2a8ac4df6f7dc0482bff44c8452fa20
YCasal-9E <casal.ca@obmontessori.edu.ph>
2025-03-09 11:11:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,17 @@ console.log("Script is loaded!");
         document.getElementById("fare-result").innerText = `Regular Ticket Price: Php ${fare}`;
     }
 }
+
+function submitForm() {
+    let name = document.getElementById("userName").value;
+    if (name.trim() === "") name = "Guest"; 
+    alert("This is noted, " + name + ".");
+}
+
+function capitalizeFirstLetter(input) {
+    let words = input.value.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    input.value = words.join(" ");
+}
